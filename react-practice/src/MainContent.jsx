727427087554e7af08324ebb1d7f5ef81b154002
wisuwat <wisuwatpond01@gmail.com>
2024-12-19 15:49:40 +0700

    Try Components

---
+++ react-practice/src/MainContent.jsx
@@ -0,0 +1,21 @@
+function MainContent () {
+    return (
+        <div>
+            <header>
+                <img src="/src/assets/react.svg" width="40px" alt="React logo" />
+            </header>
+            <main>
+                <h1>Reason I am excited to learn React</h1>
+                <ol>
+                    <li>React is a popular library, so I will be able to fit in with all the coolest devs out there</li>
+                    <li>I am more likely to get a job as a front end developer if i know React</li>
+                </ol>
+            </main>
+            <footer>
+                <small>2024 Ziroll development. All rights reserved.</small>
+            </footer>
+        </div>
+    )
+}
+
+export default MainContent;
