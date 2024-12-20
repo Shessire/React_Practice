b7121cf5b7b4988065089e93f12826591de9aa10
wisuwat <wisuwatpond01@gmail.com>
2024-12-20 12:45:27 +0700

    Finish Header Component

--- travel-journal-project/src/components/Header.jsx
+++ travel-journal-project/src/components/Header.jsx
@@ -2,9 +2,9 @@ import PublicIcon from '@mui/icons-material/Public';
 
 function Header () {
     return (
-        <header className="Header" style={{ backgroundColor: "red"}}>
-            <PublicIcon fontSize='large' />
-            <h1>my travel journal</h1>
+        <header className="Header">
+            <PublicIcon className='globe-icon' fontSize='large' />
+            <h1>my travel journal.</h1>
         </header>
     )
 }
