ff01198e45701f0c30450204366f9d16a3637f33
wisuwat <wisuwatpond01@gmail.com>
2024-12-30 15:09:42 +0700

    Create Header

---
+++ chef-claude-project/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import LunchDiningIcon from '@mui/icons-material/LunchDining';
+
+function Header () {
+    return (
+        <div className='Header'>
+            <LunchDiningIcon fontSize='large' className='logo'/>
+            <h1>Chef Claude</h1>
+        </div>
+    )
+}
+
+export default Header;
