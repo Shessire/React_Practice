6906744e2bc9f0aff1c9ab6ac71cb3b9d9821a13
wisuwat <wisuwatpond01@gmail.com>
2024-12-31 21:23:22 +0700

    Add Array State to Form

--- chef-claude-project/src/components/Main.jsx
+++ chef-claude-project/src/components/Main.jsx
@@ -2,17 +2,40 @@ import { useState } from "react";
 
 function Main () {
 
+    const [ingredient, setIngredient] = useState([])
+    const [inputValue, setInputValue] = useState("")
+
+    function handleOnChange (e) {
+        setInputValue(e.target.value)
+    }
+
+    function handleSubmit (e) {
+        e.preventDefault()
+        if (inputValue.trim()) {
+            setIngredient((prevIngredients) => [...prevIngredients, inputValue]);
+            setInputValue("");
+        }
+
+    }
+
     return (
         <div className="Main">
-            <form className="add-ingredient-form">
+            <form onSubmit={handleSubmit} className="add-ingredient-form">
                 <input 
                     type="text"
                     placeholder="e.g. pepper"
                     aria-label="Add ingredient"
-                    name="ingredient" 
+                    name="ingredient"
+                    value={inputValue}
+                    onChange={handleOnChange} 
                 />
                 <button>Add ingredient</button>
             </form>
+            <ul>
+                {ingredient.map((i, index) => (
+                    <li key={index}>{i}</li>
+                ))}
+            </ul>
         </div>
     )
 }
