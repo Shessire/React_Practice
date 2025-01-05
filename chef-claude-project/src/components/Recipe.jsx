50b97349c84bd2ab931bce6a3feb560501e5358f
wisuwat <wisuwatpond01@gmail.com>
2025-01-05 16:22:08 +0700

    Add Style to Project (Finished)

--- chef-claude-project/src/components/Recipe.jsx
+++ chef-claude-project/src/components/Recipe.jsx
@@ -1,7 +1,12 @@
+import Markdown from 'react-markdown'
+
 export default function Recipe ({ recipe }) {
     return (
-        <div>
-            {recipe}
+        <div className='suggested-recipe-container' aria-live='polite'>
+            <h2>Chef Recommends:</h2>
+            <Markdown>
+                {recipe}
+            </Markdown>
         </div>
     )
 }
